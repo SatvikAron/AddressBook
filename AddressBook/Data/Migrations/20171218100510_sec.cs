0a4d59aef780e3fa5a28d430c2b00e694d7d1c56
Jahurul Islam <jahurul1983@gmail.com>
2017-12-18 11:35:57 +0100

    Add project files.

---
+++ AddressBook/Data/Migrations/20171218100510_sec.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace AddressBook.Data.Migrations
+{
+    public partial class sec : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Adress",
+                table: "Persons",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Adress",
+                table: "Persons");
+        }
+    }
+}
---
+++ AddressBook/Models/Person.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AddressBook.Models
+{
+    public class Person
+    {
+        public int PersonId { get; set; }
+        public string Name { get; set; }
+        public String Email { get; set; }
+        public string Adress { get; set; }
+    }
+}
